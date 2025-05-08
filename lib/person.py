9e919c585b5ebf2d84859e786684359b726fe719
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-09 00:52:32 +0300

    implements the use of attributes and properties and validating input

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,35 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name= "Jerome", job="Marketing"):
+        self.name = name
+        self.job = job
+    def get_name(self):
+        return self.__name
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self.__name= name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self.__job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self.__job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
+name = Person(name="Tim", job="Marketing")
+print(name.name)
+print(name.job)
+
+
+
+
+
